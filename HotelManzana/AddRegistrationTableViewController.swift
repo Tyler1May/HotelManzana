68812496aa70a98d4928e53f04c6149c63ca2c92
Tyler May <tylermay@Tylers-MacBook-Air.local>
2023-10-25 09:21:42 -0600

    finished project

--- HotelManzana/AddRegistrationTableViewController.swift
+++ HotelManzana/AddRegistrationTableViewController.swift
@@ -15,16 +15,100 @@ class AddRegistrationTableViewController: UITableViewController {
     @IBOutlet var checkInDatePicker: UIDatePicker!
     @IBOutlet var checkOutDateLabel: UILabel!
     @IBOutlet var checkOutDatePicker: UIDatePicker!
+    var checkInDateLabelCellIndexPath = IndexPath(row: 0, section: 1)
+    var checkOutDateLabelCellIndexPath = IndexPath(row: 2, section: 1)
+    
+    let checkInDatePickerCellIndexPath = IndexPath(row: 1, section: 1)
+    let checkOutDatePickerCellIndexPath = IndexPath(row: 3, section: 1)
+    
+    var isCheckInDatePickerVisible: Bool = false {
+        didSet {
+            checkInDatePicker.isHidden = !isCheckInDatePickerVisible
+        }
+    }
+    
+    var isCheckOutDatePickerVisible: Bool = false {
+        didSet {
+            checkOutDatePicker.isHidden = !isCheckOutDatePickerVisible
+        }
+    }
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
+        let midnightToday = Calendar.current.startOfDay(for: Date())
+        checkInDatePicker.minimumDate = midnightToday
+        checkInDatePicker.date = midnightToday
+        
+        updateDateViews()
+        
+    }
+    
+    @IBAction func datePickerChange(_ sender: Any) {
+        
+    }
+    
+    func updateDateViews() {
+        checkOutDatePicker.minimumDate = Calendar.current.date(byAdding: .day, value: 1, to: checkInDatePicker.date)
+        checkInDateLabel.text = checkInDatePicker.date.formatted(date: .abbreviated, time: .omitted)
+        checkOutDateLabel.text = checkOutDatePicker.date.formatted(date: .abbreviated, time: .omitted)
     }
 
     @IBAction func doneBarButtonItemTapped(_ sender: Any) {
         let firstName = firstNameTextField.text ?? ""
         let lastName = lastNameTextField.text ?? ""
         let email = emailTextField.text ?? ""
+        let checkInDate = checkInDatePicker.date
+        let checkOutDate = checkOutDatePicker.date
+    }
+    
+    override func tableView(_ tableView: UITableView,
+       heightForRowAt indexPath: IndexPath) -> CGFloat {
+        switch indexPath {
+        case checkInDatePickerCellIndexPath where
+           isCheckInDatePickerVisible == false:
+            return 0
+        case checkOutDatePickerCellIndexPath where
+           isCheckOutDatePickerVisible == false:
+            return 0
+        default:
+            return UITableView.automaticDimension
+        }
+    }
+    
+    override func tableView(_ tableView: UITableView,
+       estimatedHeightForRowAt indexPath: IndexPath) -> CGFloat {
+        switch indexPath {
+        case checkInDatePickerCellIndexPath:
+            return 190
+        case checkOutDatePickerCellIndexPath:
+            return 190
+        default:
+            return UITableView.automaticDimension
+        }
+    }
+    
+    override func tableView(_ tableView: UITableView,
+       didSelectRowAt indexPath: IndexPath) {
+        tableView.deselectRow(at: indexPath, animated: true)
+    
+        if indexPath == checkInDateLabelCellIndexPath &&
+           isCheckOutDatePickerVisible == false {
+            isCheckInDatePickerVisible.toggle()
+        } else if indexPath == checkOutDateLabelCellIndexPath &&
+           isCheckInDatePickerVisible == false {
+            isCheckOutDatePickerVisible.toggle()
+        } else if indexPath == checkInDateLabelCellIndexPath ||
+           indexPath == checkOutDateLabelCellIndexPath {
+            isCheckInDatePickerVisible.toggle()
+            isCheckOutDatePickerVisible.toggle()
+        } else {
+            return
+        }
+        
+        updateDateViews()
+        tableView.beginUpdates()
+        tableView.endUpdates()
     }
     
 }
